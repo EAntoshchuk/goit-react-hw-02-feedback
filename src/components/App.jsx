8980fa017415a23794ad042116ba91487fa36915
Eduard Antoshchuk <e.antoshchuk@gmail.com>
2023-05-06 01:30:24 +0300

    hw2-feedback-hadlers

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,14 +5,25 @@ export const App = () => {
     <div
       style={{
         height: '100vh',
-        display: 'flex',
+        // display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101',
       }}
     >
-      goit-react-hw-02-feedback
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-around',
+          alignItems: 'center',
+          height: '100px',
+          backgroundColor: '#484890',
+          color: '#b8ca9a',
+        }}
+      >
+        React-hw-02-feedback
+      </div>
       <div>
         <Feedback />
       </div>
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,49 +1,99 @@
 import PropTypes from 'prop-types';
+import { Component } from 'react';
 import css from './Feedback.module.css';
 
-export default function Feedback() {
-//   {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   },
-//   }
-  return (
-    <div className={css.Feedback}>
-      <h2 className={css.title}>Please leave feedback</h2>
-      <div className={css.buttons}>
-        <button type="button" className={css.btn}>
-          Good
-        </button>
-        <button type="button" className={css.btn}>
-          Neutral
-        </button>
+class Feedback extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
 
-        <button type="button" className={css.btn}>
-          Bad
-        </button>
+  handleClickGood = () => {
+    this.setState(({ good }) => {
+      return {
+        good: good + 1,
+      };
+    });
+  };
+
+  handleClickNeutral = () => {
+    this.setState(({ neutral }) => {
+      return {
+        neutral: neutral + 1,
+      };
+    });
+  };
+
+  handleClickBad = () => {
+    this.setState(({ bad }) => {
+      return {
+        bad: bad + 1,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className={css.feedback}>
+        <h2 className={css.title}>Please leave feedback</h2>
+        <div className={css.buttons}>
+          <button
+            onClick={() => this.handleClickGood(this.good)}
+            type="button"
+            className={css.btn}
+          >
+            Good
+          </button>
+          <button
+            onClick={() => this.handleClickNeutral(this.neutral)}
+            type="button"
+            className={css.btn}
+          >
+            Neutral
+          </button>
+
+          <button
+            onClick={() => this.handleClickBad(this.bad)}
+            type="button"
+            className={css.btn}
+          >
+            Bad
+          </button>
+        </div>
+        <h2>Statistics</h2>
+        <ul className={css.stats}>
+          <li className={css.stats_item}>
+            <p className={css.quantity}>Good: {this.state.good}</p>
+          </li>
+          <li className={css.stats_item}>
+            <p className={css.quantity}>Neutral: {this.state.neutral}</p>
+          </li>
+          <li className={css.stats_item}>
+            <p className={css.quantity}>Bad: {this.state.bad}</p>
+          </li>
+        </ul>
       </div>
-      <h2>Statistics</h2>
-      <ul className={css.stats}>
-        <li className={css.stats_item}>
-          <p className={css.quantity}>Good: {this.state.good}</p>
-        </li>
-        <li className={css.stats_item}>
-          <p className={css.quantity}>Neutral: {this.state.neutral}</p>
-        </li>
-        <li className={css.stats_item}>
-          <p className={css.quantity}>Bad: {this.state.bad}</p>
-        </li>
-      </ul>
-    </div>
-  );
+    );
+  }
 }
 
+export default Feedback;
+
+// export default function Feedback() {
+//   {
+//   }
+// return (
+
+// );
+// }
+
 // Feedback.propTypes = {
-//   state: PropTypes.shape({
-//     good: PropTypes.number.isRequired,
-//     neutral: PropTypes.number.isRequired,
-//     bad: PropTypes.number.isRequired,
-//   }),
+//   state: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       good: PropTypes.number.isRequired,
+//       neutral: PropTypes.number.isRequired,
+//       bad: PropTypes.number.isRequired,
+//     })
+//   ),
 // };
